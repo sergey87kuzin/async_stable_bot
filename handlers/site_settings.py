29dccc39917b8b5c8197f8e617911585b45dc3e8
Sergey <s.kuzin@stik.pro>
2024-08-06 18:11:13 +0300

    conftest

--- handlers/site_settings.py
+++ handlers/site_settings.py
@@ -5,6 +5,5 @@ from schemas.site_settings import SiteSettings
 
 
 async def get_site_settings(async_session: AsyncSession) -> SiteSettings:
-    async with async_session.begin():
-        result = await async_session.execute(select(SiteSettings))
-        return result.fetchone()[0]
+    result = await async_session.execute(select(SiteSettings))
+    return result.fetchone()[0]
