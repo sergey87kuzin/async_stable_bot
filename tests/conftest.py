959b278c9cd129a8c23f5e2c04615d2a1f7f8787
Sergey <s.kuzin@stik.pro>
2024-08-07 12:53:23 +0300

    first test with pytest.ini

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ import asyncpg
 from fastapi.testclient import TestClient
 from httpx import AsyncClient
 import pytest
-from sqlalchemy import insert
+from sqlalchemy import insert, text
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import NullPool
@@ -18,6 +18,26 @@ from settings import TEST_DATABASE_URL
 engine_test = create_async_engine(TEST_DATABASE_URL, poolclass=NullPool)
 async_session_maker = sessionmaker(engine_test, class_=AsyncSession, expire_on_commit=False)
 metadata.bind = engine_test
+pytest_plugins = ('pytest_asyncio',)
+
+CLEAN_TABLES = [
+    "bot_config_sitesettings",
+    "users_user"
+]
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+# @pytest.fixture(scope="session", autouse=True)
+# async def run_migrations():
+#     os.system("alembic init migrations")
+#     os.system('alembic revision --autogenerate -m "test running migrations"')
+#     os.system("alembic upgrade heads")
 
 
 @pytest.fixture(scope="session")
@@ -27,33 +47,13 @@ async def async_session_test():
     yield async_session
 
 
-@pytest.fixture(scope="function", autouse=True)
-async def create_sitesettings(async_session_test):
+@pytest.fixture(scope="session", autouse=True)
+async def clean_tables(async_session_test):
     """Clean data in all tables before running test function"""
     async with async_session_test() as session:
         async with session.begin():
-            await session.execute(
-                """INSERT INTO bot_config_sitesettings VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);""",
-                1,
-                300,
-                1000,
-                None,
-                200,
-                1000,
-                None,
-                None,
-                "https://ya.ru",
-                1
-            )
-
-
-@pytest.fixture(scope="session")
-async def asyncpg_pool():
-    pool = await asyncpg.create_pool(
-        "".join(TEST_DATABASE_URL.split("+asyncpg"))
-    )
-    yield pool
-    pool.close()
+            for table_for_cleaning in CLEAN_TABLES:
+                await session.execute(text(f"TRUNCATE TABLE {table_for_cleaning} CASCADE;"))
 
 
 async def _get_test_db():
@@ -85,27 +85,31 @@ async def client() -> Generator[TestClient, Any, None]:
 
 
 @pytest.fixture(scope="session")
-async def ac() -> AsyncGenerator[AsyncClient, None]:
-    async with AsyncClient(app=app, base_url="http://localhost:8000") as ac:
-        yield ac
+async def asyncpg_pool():
+    pool = await asyncpg.create_pool(
+        "".join(TEST_DATABASE_URL.split("+asyncpg"))
+    )
+    yield pool
+    pool.close()
 
 
-# @pytest.fixture
-# async def create_site_settings_in_database(asyncpg_pool):
-#     async def create_site_settings_in_database():
-#         async with asyncpg_pool.acquire() as connection:
-#             return await connection.execute(
-#                 """INSERT INTO bot_config_sitesettings VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);""",
-#                 1,
-#                 300,
-#                 1000,
-#                 None,
-#                 200,
-#                 1000,
-#                 None,
-#                 None,
-#                 "https://ya.ru",
-#                 1
-#             )
-#
-#     return create_site_settings_in_database
+@pytest.fixture(scope="session", autouse=True)
+async def create_site_settings(async_session_test):
+    """Clean data in all tables before running test function"""
+    async with async_session_test() as session:
+        async with session.begin():
+            await session.execute(
+                insert(SiteSettings).values(
+                    {
+                        "day_tariff_cost": 300,
+                        "month_tariff_cost": 1000,
+                        "day_tariff_count": 300,
+                        "month_tariff_count": 1000,
+                        "say_hi_video": None,
+                        "say_hi_video_video_bot": None,
+                        "notice_message": None,
+                        "settings_lesson_link": "https://ya.ru",
+                        "queue_number": 1
+                    }
+                )
+            )
