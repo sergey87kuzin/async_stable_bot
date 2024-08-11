3c1a8390e16bb560ab90397df1cc400dd52488fc
Sergey <s.kuzin@stik.pro>
2024-08-11 15:37:42 +0300

    text message test no request

--- tests/test_text_message.py
+++ tests/test_text_message.py
@@ -3,10 +3,14 @@ import pytest
 from telegram_message import text_message
 
 
-@pytest.mark.asyncio
-async def test_text_message(client, get_user_from_database):
+async def test_text_message(client, create_user_in_database):
     request_json = text_message
     request_json["text"] = "some interesting prompt"
     response = client.post('/telegram', json=request_json)
 
-    assert response.status_code == 204, "Запрос на старт чата дает неправильный статус"
+    assert response.status_code == 204, "Неверный статус при недостаточном числе генераций"
+
+    await create_user_in_database("SergeyAKuzin")
+    response = client.post('/telegram', json=request_json)
+
+    assert response.status_code == 200, "Неверный статус при достаточном числе генераций"
