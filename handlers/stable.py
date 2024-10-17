16a197c168a9accf26018680b61d825005983ee3
Sergey <s.kuzin@stik.pro>
2024-10-17 19:35:44 +0300

    fetch messages

--- handlers/stable.py
+++ handlers/stable.py
@@ -90,10 +90,8 @@ async def send_upscale_to_stable(
 async def fetch_message(message: StableMessage, user: User, session: AsyncSession) -> Union[StableMessage, None]:
     """https://docs.modelslab.com/image-generation/community-models/dreamboothfetchqueimg"""
     from helpers import handle_stable_fetch_answer
-    data = {
-        "key": STABLE_API_KEY,
-        "request_id": message.stable_request_id
-    }
+    if not message.stable_request_id:
+        return None
     if message.message_type == StableMessageTypeChoices.FIRST:
         fetch_url = f"https://modelslab.com/api/v6/images/fetch"
         data = {
